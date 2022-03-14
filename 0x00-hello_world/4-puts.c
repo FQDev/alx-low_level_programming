77dd164514e69704194ba05a1c42c1eaa1d3054e
FQDev <fquouhadiho@gmail.com>
2022-03-14 16:53:24 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdio.h>
+/**
+ * main function - Entry point
+ *
+ * Return 0
+ * 
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
- 	return (0);
+	return (0);
 }
