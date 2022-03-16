643706d4d78ef1f3861304286cd160f1fc54a2d6
FQDev <fquouhadiho@gmail.com>
2022-03-15 20:39:48 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *  _islower - check the code
+ *
+ * Return: 1 if true and 0 if false.
+ */
+int _islower(int c)
+{
+	if( islower(c) > 0 )
+	{
+		return 1;
+	}
+       	
+	else 
+	
+	{
+		return 0;
+	}
+}
