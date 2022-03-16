c47cb57fc44894f60c57d6d38f532062d9219e03
FQDev <fquouhadiho@gmail.com>
2022-03-15 21:59:37 -0700

    last dig

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - check the code
+ * @c: param to be checked
+ * Return: last digit of @c.
+ */
+int print_last_digit(int c)
+{
+	char s[] = c+"";
+	int size = (int)sizeof(s) / sizeof(s[0]);
+	char lastD[1] = s[size - 1];
+	
+	return (lastD[0]);
+}
