453851404dbb381afbbe10562d181b8dc81b4522
FQDev <fquouhadiho@gmail.com>
2022-03-15 21:23:23 -0700

    char

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <ctype.h>
 /**
- *  _print_sign - check the code
+ *  print_sign - check the code
  * @n: param to be checked
  * Return: 1 and prints + if n is greater than zero, 
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int _print_sign(int n)
+int print_sign(int n)
 {
 	char res[3] = "+0-";
 	if (n > 0)
