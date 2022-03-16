f5b1f3fb6482f93ed62b7d9ac20494109d8ae77a
FQDev <fquouhadiho@gmail.com>
2022-03-15 20:19:22 -0700

    alpha_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "print_alphabet.c"
+
+/**
+ *  print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		print_alphabet();
+		_putchar('\n');
+	}
