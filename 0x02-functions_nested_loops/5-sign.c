027d54fe4528291233ed3ee0e5fafae486987774
FQDev <fquouhadiho@gmail.com>
2022-03-15 21:29:34 -0700

    betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,12 +4,12 @@
 /**
  * print_sign - check the code
  * @n: param to be checked
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero -1 and prints - if n is less than zero
+ * Return: 1 or 0 or -1
  */
 int print_sign(int n)
 {
 	char res[3] = "+0-";
-	
+
 	if (n > 0)
 	{
 		_putchar(res[0]);
