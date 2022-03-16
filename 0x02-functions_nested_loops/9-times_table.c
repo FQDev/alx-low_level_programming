710207158e990167eba9a89d3a0668c65e34901c
FQDev <fquouhadiho@gmail.com>
2022-03-15 22:57:49 -0700

    corr

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,18 +10,36 @@ void times_table(void)
 {
 	int j = 0;
 	int i = 0;
+	int size = 1;
 
 	for (i = 0; i < 10; i++)
 	{
 		int last = i * 9;
 
-		printf("%d", i);
 		for (j = 0; j < 9; j++)
 		{
 			int res = i * j;
-			printf(",  %d", res);
+			char s[] = res+"";
 
+			if (sizeof(s) / sizeof(s[0]) == 1)
+			{
+				printf(",  %d", res);
+			}
+			else
+			{
+				printf(", %d", res);
+			}
 		}
-		printf(",  %d$\n", last);
+
+		char s[] = last+"";
+		
+		if (sizeof(s) / sizeof(s[0]) == 1)
+		{
+			printf(",  %d\n", last);
+		}
+		else
+		{
+			printf(", %d\n", last);
+		}
 	}
 }
