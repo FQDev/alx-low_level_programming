7c4327df40826bd2c816ba20ac4c798aabe309d6
FQDev <fquouhadiho@gmail.com>
2022-03-17 02:30:48 -0700

    lastD

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 
 /**
  * print_last_digit - check the code
@@ -11,36 +10,8 @@
  */
 int print_last_digit(int c)
 {
-	/**
-	 * char s[12] = "";
-	 * int size = 0;
-	 * char lastD[2] = "";
-	 *
-	 * int lastDig = 0;
-	 *
-	 * snprintf(s, 12, "%d", c);
-	 * size = (int)sizeof(s) / sizeof(s[0]);
-	 * lastD[2] =  s[size - 1];
-	 *
-	 * lastDig = c % 10;
-	 * snprintf(s, 12, "%d%d", lastDig, lastDig);
-	 * s[0] = lastDig;
-	 * s[1] = lastDig;
-	 */
-	int num = abs(c) % 10;
-	 /* int retval = num * pow(10, (int)log10(num)+1) + num;*/
-	char s1[2];
-	/**
-	 * char s2[20];
-	 * int retval = 0;
-	 *
-	 * sprintf(s1, "%d", num);
-	 * sprintf(s2, "%d", num);
-	 * strcat(s1, s2);
-	 * retval = atol(s1);
-	 */
-	s1[0] = num;
-	s1[1] = num;
+	int lastD = 0;
 
-	return (atoi(s1));
+	lastD = abs(c) % 10;
+	return (lastD);
 }
