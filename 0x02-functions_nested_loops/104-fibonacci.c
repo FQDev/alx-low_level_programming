01f10cc786425b8200df662ffba8a066e1a9d90f
FQDev <fquouhadiho@gmail.com>
2022-03-17 10:53:11 -0700

    fibo4

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - check the code
+ * Return: 0.
+ */
+int main(void)
+{
+	unsigned long n1 = 1, n2 = 2, sum = 3, compt = 2;
+
+	printf("%lu, %lu, ", n1, n2);
+	while (compt < 98)
+	{
+		sum = n1 + n2;
+		printf("%lu, ", sum);
+		n1 = n2;
+		n2 = sum;
+		compt++;
+	}
+	printf("%lu\n", n1 + n2);
+	return (0);
+}
