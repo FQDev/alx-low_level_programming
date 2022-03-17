17d4311fa24573f8d4cbc4c27c883504079008e5
FQDev <fquouhadiho@gmail.com>
2022-03-17 09:14:35 -0700

    long u

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	long i = 0, n1 = 1, n2 = 2, sum = 0;
+	unsigned long i = 0, n1 = 1, n2 = 2, sum = 0;
 	
-	printf("%d, %d, ", n1, n2);
+	printf("%lu, %lu, ", n1, n2);
 	for (i = 3; i < 50; i++)
 	{
 		sum = n1 + n2;
-		printf("%d, ", sum);
+		printf("%lu, ", sum);
 		n1 = n2;
 		n2 = sum;
 	}
