0cec306505de085df79938b38e89acfb31bc394e
FQDev <fquouhadiho@gmail.com>
2022-03-16 21:47:58 -0700

    ok

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,25 +11,8 @@ void jack_bauer(void)
 	int m = 0;
 	int s = 0;
 
-	/* printf("00:00\n"); */
 	for (m = 0; m < 24; m++)
 	{
-		/*
-		 *
-		 * if (m == 24)
-		 * {
-		 *	break;
-		 * }
-		 * if (s == 59)
-		 * {
-		 *	s = 0;
-		 *	m++;
-		 * }
-		 * else
-		 * {
-		 *	s++;
-		 * }
-		 */
 		for (s = 0; s < 60; s++)
 		{
 			if (s >= 10 && m >= 10)
