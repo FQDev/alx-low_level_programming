0c20831ad8252bba569d8e108b58b632fb936fc5
FQDev <fquouhadiho@gmail.com>
2022-03-17 12:16:08 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 98; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
