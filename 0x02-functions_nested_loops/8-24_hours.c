b66c1943a01a10ef3ed00bc55dbb76badeed5835
FQDev <fquouhadiho@gmail.com>
2022-03-16 21:46:12 -0700

    for

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,39 +11,45 @@ void jack_bauer(void)
 	int m = 0;
 	int s = 0;
 
-	printf("00:00\n");
-	while (m < 24)
+	/* printf("00:00\n"); */
+	for (m = 0; m < 24; m++)
 	{
-		if (m == 24)
+		/*
+		 *
+		 * if (m == 24)
+		 * {
+		 *	break;
+		 * }
+		 * if (s == 59)
+		 * {
+		 *	s = 0;
+		 *	m++;
+		 * }
+		 * else
+		 * {
+		 *	s++;
+		 * }
+		 */
+		for (s = 0; s < 60; s++)
 		{
-			break;
+			if (s >= 10 && m >= 10)
+			{
+				printf("%d:%d", m, s);
+			}
+			else if (s >= 10 && m < 10)
+			{
+				printf("0%d:%d", m, s);
+			}
+			else if (s < 10 && m < 10)
+			{
+				printf("0%d:0%d", m, s);
+			}
+			else
+			{
+				printf("%d:0%d", m, s);
+			}
+			printf("\n");
 		}
-		if (s == 59)
-		{
-			s = 0;
-			m++;
-		}
-		else
-		{
-			s++;
-		}
-		if (s >= 10 && m >= 10)
-		{
-			printf("%d:%d", m, s);
-		}
-		else if (s >= 10 && m < 10)
-		{
-			printf("0%d:%d", m, s);
-		}
-		else if (s < 10 && m < 10)
-		{
-			printf("0%d:0%d", m, s);
-		}
-		else
-		{
-			printf("%d:0%d", m, s);
-		}
-		printf("\n");
 
 	}
 }
