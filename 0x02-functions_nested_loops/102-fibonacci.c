81827815b6b383d9e0513bb6fc41610621993591
FQDev <fquouhadiho@gmail.com>
2022-03-17 09:03:17 -0700

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: 0.
+ */
+int main(void)
+{
+	int i = 0, n1 = 1, n2 = 2, sum = 0;
+	
+	printf("%d, %d, ");
+	for (i = 3; i < 50; i++)
+	{
+		sum = n1 + n2;
+		printf("%d, ", sum);
+		n1 = n2;
+		n2 = sum;
+	}
+	/* printf("%d\n", sum); */
+	return (0);
+}
