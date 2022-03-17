c1fe1aabb279ac63658d4fc910e685bb09061e14
FQDev <fquouhadiho@gmail.com>
2022-03-17 10:12:32 -0700

    ok

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,10 +12,10 @@ int main(void)
 	unsigned long n1 = 1, n2 = 2, sum = 5;
 
 	/* printf("%lu, %lu, 3, ", n1, n2); */
-	while (n1 + n2 < 5000000)
+	while (n1 + n2 <= 4999999)
 	{
 		sum = n1 + n2;
-	        /**
+		/**
 		 * printf("%lu, ", sum);
 		 */
 		n1 = n2;
