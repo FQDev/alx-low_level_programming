9d67a3a7fb0f2f16da417856bd51ab2335331ffd
FQDev <fquouhadiho@gmail.com>
2022-03-17 11:56:05 -0700

    OkAa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
